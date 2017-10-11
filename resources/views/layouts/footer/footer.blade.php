d9e4927a641897a9496ea74522fbdf221e087e84
Mohammed Ibrahim <mohibrahimqop@gmail.com>
2017-10-11 16:03:30 +0200

    daily commit

--- resources/views/layouts/footer/footer.blade.php
+++ resources/views/layouts/footer/footer.blade.php
@@ -1,45 +1,60 @@
 
 <div id="footer-container" class="container-fluid">
-
 	<div class="container">
 		
-		
-		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-			<img src="{{asset('images/footer/AOE-Logo-White.png')}}" class="img-responsive" alt="Image">
-		</div>
+	
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >			
+			
 
-		<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-			<ul class="footer-nav-ul">
-				<li>
-					<a href="#" class="footer-nav-a">Home</a>					
-				</li>
-				<li>
-					<a href="#" class="footer-nav-a">Parts And Maintenance</a>					
-				</li>
-				<li>
-					<a href="#" class="footer-nav-a">Contact Us</a>					
-				</li>
-			</ul>
-		</div>
 
-		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-			<a href="#" class="footer-nav-a">Product Lineup</a>
-			<ul class="footer-nav-ul">
-				<li><a href="#">23 PPM and Up</a></li>
-                <li><a href="#">22 PPM Models</a></li>
-                <li><a href="#">HV Production MFP</a></li>
-			</ul>
-		</div>
 
-		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-			<a href="#" class="footer-nav-a">Solutions</a>
-			<ul class="footer-nav-ul">
-				<li><a href="#">Office Solutions</a></li>
-			</ul>
-		</div>
+			<div class="col-xs-4 col-xs-offset-4		col-sm-2 col-sm-offset-0    col-md-2  col-md-offset-0		col-lg-2 col-lg-offset-0">
+				<img src="{{asset('images/branding_image.png')}}" class="img-responsive" alt="Image">
+			</div>
+			
+			<span>
+				<div class="col-xs-8 col-xs-offset-4		col-sm-4 col-sm-offset-0		col-md-3 col-md-offset-0		col-lg-2 col-lg-offset-0">
+					<ul class="footer-nav-ul-1">
+						<li>
+							<a href="#" class="footer-nav-a">Home</a>					
+						</li>
+						<li>
+							<a href="#" class="footer-nav-a">Parts And Maintenance</a>					
+						</li>
+						<li>
+							<a href="#" class="footer-nav-a">Contact Us</a>					
+						</li>
+					</ul>
+				</div>
 
-		
-		
+				<div class="col-xs-8 col-xs-offset-4		col-sm-3 col-sm-offset-0     col-md-2 col-md-offset-0      col-lg-2 col-lg-offset-0">
+					<a href="#" class="footer-nav-a">Product Lineup</a>
+					<ul class="footer-nav-ul">
+						<li><a href="#">23 PPM and Up</a></li>
+		                <li><a href="#">22 PPM Models</a></li>
+		                <li><a href="#">HV Production MFP</a></li>
+					</ul>
+				</div>
+
+				<div class="col-xs-8 col-xs-offset-4		col-sm-2 col-sm-offset-0     col-md-2 col-md-offset-0      col-lg-2 col-lg-offset-0">
+					<a href="#" class="footer-nav-a">Solutions</a>
+					<ul class="footer-nav-ul">
+						<li><a href="#">Office Solutions</a></li>
+					</ul>
+				</div>
+
+				<div class="col-xs-8 col-xs-offset-4		col-sm-4 col-sm-offset-5    col-md-3 col-md-offset-0      col-lg-3 col-lg-offset-1" style="border-left: 2px solid #282C72">
+					<p class="navbar-text">                                          
+		                <a href="#" class="navbar-link fa fa-facebook" ></a>
+		                <a href="#" class="navbar-link fa fa-twitter"></a>
+		                <a href="#" class="navbar-link fa fa-google-plus"></a>
+		            </p> 
+				</div>
+			</span>
+				
+
+			
+		</div>
 		
 	</div>
 </div>
